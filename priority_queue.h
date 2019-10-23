3daef28829a2cb0dd1c58f491a047eb8bef2c36f
greatose <56288955+greatose@users.noreply.github.com>
2019-10-23 16:12:39 +0900

    Update priority_queue.h
    Class style 가이드에 따른 수정

--- priority_queue.h
+++ priority_queue.h
@@ -1,15 +1,22 @@
+#ifndef PRIORITY_QUEUE_H_
+#define PRIORITY_QUEUE_H_
+
 #include <queue>
 
 template <typename T> 
 class templatePriorityQueue {
-	private:
-		std::priority_queue<T> storage;
-	public:
-		templatePriorityQueue();
-		~templatePriorityQueue();
-		bool empty() const;
-		const T& top() const;
-		int size();
-		void push(const T&);
-		void pop();	
+ public:
+  templatePriorityQueue();
+  ~templatePriorityQueue();
+  bool empty() const;
+  const T& top() const;
+  int size();
+
+  void push(const T&);
+  void pop();
+
+private:
+ std::priority_queue<T> storage;
 };
+
+#endif // PRIORITY_QUEUE_H_
